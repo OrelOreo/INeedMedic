62786cc231fcd5ca09c9e90345cfc43fd57518ee
OrelOreo <oreloreopro@gmail.com>
2025-12-29 20:04:19 +0100

    chore: configuration for connecting to the database

---
+++ db/prisma.ts
@@ -0,0 +1,10 @@
+import { neonConfig } from "@neondatabase/serverless";
+import { PrismaNeon } from "@prisma/adapter-neon";
+import { PrismaClient } from "@prisma/client";
+import ws from "ws";
+
+neonConfig.webSocketConstructor = ws;
+
+const connectionString = process.env.DATABASE_URL!;
+const adapter = new PrismaNeon({ connectionString });
+export const prisma = new PrismaClient({ adapter });
