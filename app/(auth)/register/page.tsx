b4d42d2c8dd066285438af089c72720e77d513a9
OrelOreo <oreloreopro@gmail.com>
2025-12-30 19:34:12 +0100

    feat(metadata): add (login page, register page and default layout) metadata

--- app/(auth)/register/page.tsx
+++ app/(auth)/register/page.tsx
@@ -1,5 +1,10 @@
 import { Suspense } from "react";
 import RegisterForm from "@/components/register-form";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Register",
+};
 
 export default function RegisterPage() {
   return (
