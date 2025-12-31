642b9419eea47ed53affd755a038bc2d539b81c9
OrelOreo <oreloreopro@gmail.com>
2025-12-31 20:01:22 +0100

    feat: add get appointments route

---
+++ app/api/appointments/route.ts
@@ -0,0 +1,43 @@
+// app/api/appointments/route.ts
+import { NextResponse } from "next/server";
+import { getServerSession } from "next-auth";
+import { authOptions } from "../auth/[...nextauth]/route";
+import { prisma } from "@/db/prisma";
+
+export async function GET() {
+  const session = await getServerSession(authOptions);
+
+  if (!session) {
+    return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+  }
+
+  const userId = session.user.id;
+  const practitionerId = session.user.practitionerId;
+  const role = session.user.role; // "client" ou "practitioner"
+
+  let whereClause = {};
+
+  if (role === "PRACTITIONER") {
+    whereClause = { practitionerId: practitionerId };
+  } else if (role === "CLIENT") {
+    whereClause = { clientId: userId };
+  } else {
+    return NextResponse.json(
+      { error: "Forbidden: unknown role" },
+      { status: 403 }
+    );
+  }
+
+  const appointments = await prisma.appointment.findMany({
+    where: whereClause,
+    include: {
+      client: true,
+      practitioner: true,
+    },
+    orderBy: {
+      startDateTime: "asc",
+    },
+  });
+
+  return NextResponse.json({ appointments });
+}
