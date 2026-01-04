018210f51223b76750e0b20e78fd40779a6f6c48
OrelOreo <oreloreopro@gmail.com>
2026-01-04 19:51:34 +0100

    feat: add utility function for formatting date

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,17 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+export function formatDate(date: Date) {
+  return new Date(date).toLocaleDateString("fr-FR");
+}
+
+export function formatTime(date: Date) {
+  return new Date(date).toLocaleTimeString("fr-FR", {
+    hour: "2-digit",
+    minute: "2-digit",
+  });
 }
